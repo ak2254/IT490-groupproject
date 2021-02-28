ba7df2d324f36d3cbf06589cb550637195f54363
Thomas Semiz <tas56@njit.edu>
2021-02-27 22:34:04 -0500

    Fixed front end to serve the correct HTML pages. Began connecting backend reading and writing to database

--- back-end/app/server.js
+++ back-end/app/server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-
+const https = require('https');
 
 // Messaging configuration
 const messaging_host = 'messaging';
@@ -9,6 +9,16 @@ const messaging_pass = process.env.MSG_PASS;
 const queue = 'requests';
 const messaging_url = `amqp://${messaging_user}:${messaging_pass}@${messaging_host}`;
 const amqp = require('amqplib/callback_api');
+
+// Database configuration
+const db_host = 'db';
+const db_user = process.env.POSTGRES_USER;
+const db_pass = process.env.POSTGRES_PASSWORD;
+const db_database = process.env.POSTGRES_DB;
+const postgres = require('pg');
+const Pool = postgres.Pool;
+const db_pool = new Pool ({host: db_host, user: db_user,
+  password: db_pass, database: db_database});
 
 
 
@@ -37,6 +47,32 @@ amqp.connect(messaging_url, function(error0, connection) {
       send_response(channel, msg, {'status': 'OK', 'data': data});
 
       var received_json = JSON.parse(msg.content);
+
+      // Read and write to db
+
+
+      const getUser = (request, response) => {
+        const id = parseInt(request.params.id)
+
+        db_pool.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
+          if (err) {
+            throw err
+          }
+          response.status(200).json(res.rows);
+          db_pool.end();
+        })
+      }
+
+      const createUser = (request, response) => {
+        const { name, email } = request.body
+
+        db_pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (err, res) => {
+          if (err) {
+            throw err
+          }
+          response.status(201).send(`User added with ID: ${res.insertId}`)
+        })
+      }
 
 
     });
--- front-end/server.js
+++ front-end/server.js
@@ -4,19 +4,16 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'app')));
+
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
+
+app.get('/NewUserPage.html', (req, res) => {
+    res.sendFile('NewUserPage.html');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
-
-// const http = require('http');
-// const fs = require('fs');
-// const server = http.createServer((request, re) => {
-//     re.writeHead(200, { 'content-type': 'text/html' });
-//     fs.createReadStream('app/index.html').pipe(re)
-// })
-//
-// server.listen(process.env.PORT || 3000);
 
