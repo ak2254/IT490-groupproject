d2e5db6a0493551ef9276e1e95eb8db7e27748d4
krb39 <krb39@njit.edu>
2021-03-21 21:25:51 +0530

    issue #17 removed commented import

--- front_end/app.py
+++ front_end/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, session, redirect
 from functools import wraps
-# from werkzeug.security import check_password_hash, generate_password_hash, gen_salt
 import werkzeug
 import logging
 import messaging
